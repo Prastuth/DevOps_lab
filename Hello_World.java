8df76fc9b505e542281e86b2275d8e8941b964cf
Prastuth <prastuthjain19@gmail.com>
2022-11-23 10:54:07 +0530

    message

---
+++ Hello_World.java
@@ -0,0 +1,8 @@
+public class Hello_World{
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.println("Hello World!");
+        }
+        
+    }
+}
